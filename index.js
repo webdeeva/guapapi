80adc84ccbfac7acfc433a7e0c1316f1ae76a81f
webdeeva <tavonia@gmail.com>
2024-02-20 19:44:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get('/balance/:address', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
